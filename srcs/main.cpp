568347b4a8b01bb04b472c5910aa85de597fc445
yuhe-02 <domineter4649@gmail.com>
2026-05-18 00:10:33 +0900

    Refactor main.cpp to improve window creation and event handling
    - Added OpenGL context version hints for better compatibility.
    - Implemented a check_if_close_window function to handle window closing via the ESC key.
    - Enhanced error handling for window creation and GLAD initialization.
    - Updated the main loop to include event polling and buffer swapping.

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,13 +1,47 @@
 #include <cstdlib>
 #include <iostream>
+#include <string>
+#include <vector>
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main() {
-  glfwInit();
 
-  GLFWwindow *window = glfwCreateWindow(800, 600, "scop",nullptr, nullptr);
+void check_if_close_window(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+    return;
+}
 
-  glfwMakeContextCurrent(window);
-  glfwSwapInterval(1);
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // need on MacOS
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Faild to create glfw window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+
+    while(!glfwWindowShouldClose(window)) {
+        check_if_close_window(window);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
 }
