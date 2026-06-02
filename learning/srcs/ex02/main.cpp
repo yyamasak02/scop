36128db0f41b4483c943a6edab95f2538ce2d6d5
yuhe-02 <domineter4649@gmail.com>
2026-06-03 00:43:45 +0900

    feat: 複数の三角形を描画する機能を追加

--- learning/srcs/ex02/main.cpp
+++ learning/srcs/ex02/main.cpp
@@ -110,16 +110,26 @@ int main() {
       0.0f,  0.5f,  0.0f   // top
   };
 
-  unsigned int VBO, VAO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
+  float vertices2[] = {
+      0.0f,  -0.5f, 0.0f,  // left
+      0.9f,  -0.5f, 0.0f,  // right
+      0.45f, 0.5f,  0.0f   // top
+  };
+
+  unsigned int VBO[2], VAO[2];
+  glGenVertexArrays(2, VAO);
+  glGenBuffers(2, VBO);
   // bind the Vertex Array Object first, then bind and set vertex buffer(s), and
   // then configure vertex attributes(s).
-  glBindVertexArray(VAO);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBindVertexArray(VAO[0]);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
 
+  glBindVertexArray(VAO[1]);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
@@ -151,9 +161,14 @@ int main() {
 
     // draw our first triangle
     glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);  // seeing as we only have a single VAO there's no
-                             // need to bind it every time, but we'll do so to
-                             // keep things a bit more organized
+    glBindVertexArray(VAO[0]);  // seeing as we only have a single VAO there's
+                                // no need to bind it every time, but we'll do
+                                // so to keep things a bit more organized
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glBindVertexArray(VAO[1]);  // seeing as we only have a single VAO there's
+                                // no need to bind it every time, but we'll do
+                                // so to keep things a bit more organized
     glDrawArrays(GL_TRIANGLES, 0, 3);
     // glBindVertexArray(0); // no need to unbind it every time
 
@@ -166,8 +181,8 @@ int main() {
 
   // optional: de-allocate all resources once they've outlived their purpose:
   // ------------------------------------------------------------------------
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteBuffers(1, &VBO);
+  glDeleteVertexArrays(2, VAO);
+  glDeleteBuffers(2, VBO);
   glDeleteProgram(shaderProgram);
 
   // glfw: terminate, clearing all previously allocated GLFW resources.
