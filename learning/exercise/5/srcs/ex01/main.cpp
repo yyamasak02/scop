a8df833e96bb3612dc4a46de9eeaac4243ad7c3d
yuhe-02 <domineter4649@gmail.com>
2026-06-07 10:37:26 +0900

    feat: OpenGL用のシェーダーファイルを追加

--- learning/exercise/5/srcs/ex01/main.cpp
+++ learning/exercise/5/srcs/ex01/main.cpp
@@ -47,7 +47,8 @@ int main() {
 
   // build and compile our shader zprogram
   // ------------------------------------
-  Shader ourShader("4.2.texture.vs", "4.2.texture.fs");
+  Shader ourShader("resources/glsl/4.2.texture.vs",
+                   "resources/glsl/4.2.texture.fs");
 
   // set up vertex data (and buffer(s)) and configure vertex attributes
   // ------------------------------------------------------------------
@@ -109,9 +110,8 @@ int main() {
       true);  // tell stb_image.h to flip loaded texture's on the y-axis.
   // The FileSystem::getPath(...) is part of the GitHub repository so we can
   // find files on any IDE/platform; replace it with your own image path.
-  unsigned char* data =
-      stbi_load("resources/textures/container.jpg",
-                &width, &height, &nrChannels, 0);
+  unsigned char* data = stbi_load("resources/textures/container.jpg", &width,
+                                  &height, &nrChannels, 0);
   if (data) {
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
                  GL_UNSIGNED_BYTE, data);
@@ -133,9 +133,8 @@ int main() {
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   // load image, create texture and generate mipmaps
-  data = stbi_load(
-      "resources/textures/awesomeface.png", &width,
-      &height, &nrChannels, 0);
+  data = stbi_load("resources/textures/awesomeface.png", &width, &height,
+                   &nrChannels, 0);
   if (data) {
     // note that the awesomeface.png has transparency and thus an alpha channel,
     // so make sure to tell OpenGL the data type is of GL_RGBA
